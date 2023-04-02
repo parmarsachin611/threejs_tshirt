5692c1bc571014dfd87973f4e538e0b47ed3ad98
sachinparmar611 <parmarsachin707@gmail.com>
2023-04-02 12:48:42 +0530

    Update Link

--- server/index.js
+++ server/index.js
@@ -5,8 +5,14 @@ import dalleRoutes from "./routes/dalle.routes.js"
 
 dotenv.config();
 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
